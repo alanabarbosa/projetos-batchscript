a4b6aa6b85c21a229a9c9eb5693f80ab674bca77
Alana Barbosa <alana@prumotecnologia.com.br>
2024-02-05 09:07:02 -0300

    Fix bug

--- estrutura-condicional-00.bat
+++ estrutura-condicional-00.bat
@@ -21,53 +21,53 @@ REM  QBFC Project Options End
 chcp 1252 > nul
 color 02
 
-set /p number="Digite um n˙mero> "
-set /p number2="Digite um novo n˙mero> "
+set /p number="Digite um n√∫mero> "
+set /p number2="Digite um novo n√∫mero> "
 
 IF %number% EQU %number2% (
-   echo %number% È igual a %number2%
+   echo %number% √© igual a %number2%
 ) ELSE (
-  echo %number% n„o È igual a %number2%
+  echo %number% n√£o √© igual a %number2%
 )
 
 echo.
 
 IF %number% NEQ %number2% (
-   echo %number% È diferente a %number2%
+   echo %number% √© diferente a %number2%
 ) ELSE (
-  echo %number% n„o È diferente a %number2%
+  echo %number% n√£o √© diferente a %number2%
 )
 
 echo.
 
 IF %number% LSS %number2% (
-   echo %number% È menor %number2%
+   echo %number% √© menor %number2%
 ) ELSE (
-  echo %number% n„o È menor %number2%
+  echo %number% n√£o √© menor %number2%
 )
 
 echo.
 
 IF %number% LEQ %number2% (
-   echo %number% È menor ou igual que %number2%
+   echo %number% √© menor ou igual que %number2%
 ) ELSE (
-  echo %number% n„o È menor ou igual que %number2%
+  echo %number% n√£o √© menor ou igual que %number2%
 )
 
 echo.
 
 IF %number% GTR %number2% (
-   echo %number% È maior que %number2%
+   echo %number% √© maior que %number2%
 ) ELSE (
-  echo %number% n„o È maior que %number2%
+  echo %number% n√£o √© maior que %number2%
 )
 
 echo.
 
 IF %number% GEQ %number2% (
-   echo %number% È maior ou igual que %number2%
+   echo %number% √© maior ou igual que %number2%
 ) ELSE (
-  echo %number% n„o È maior ou igual que %number2%
+  echo %number% n√£o √© maior ou igual que %number2%
 )
 
 echo.
