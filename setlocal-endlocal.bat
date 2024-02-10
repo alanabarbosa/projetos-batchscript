1cccb3f258b040835c03621b53976195904608bd
Alana Barbosa <alana@prumotecnologia.com.br>
2024-02-10 00:16:09 -0300

    Add new bachfile

---
+++ setlocal-endlocal.bat
@@ -0,0 +1,37 @@
+@ECHO OFF
+REM  QBFC Project Options Begin
+REM  HasVersionInfo: No
+REM Companyname: 
+REM Productname: 
+REM Filedescription: 
+REM Copyrights: 
+REM Trademarks: 
+REM Originalname: 
+REM Comments: 
+REM Productversion:  0. 0. 0. 0
+REM Fileversion:  0. 0. 0. 0
+REM Internalname: 
+REM ExeType: consoleold
+REM Architecture: x64
+REM Appicon: 
+REM AdministratorManifest: No
+REM  QBFC Project Options End
+@ECHO ON
+@echo off
+chcp 1252 > nul
+setlocal
+
+set /a A=2
+set /a B=3
+set /a "C= A * B"
+
+for /f %%a in ("%C%") do (
+   endlocal
+   set r=%%a
+)
+
+echo %r%
+echo A=%A% B=%B% C=%C%
+
+
+pause > nul
